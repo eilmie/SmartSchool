f7188b781390e74b08231b5c8612d4f4767b717a
Muhammad Eilmie <muhammadeilmie@gmail.com>
2026-01-19 14:20:12 +0800

    adminhomepage.html

--- src/main/java/com/smartschool/controller/AdminStatsServlet.java
+++ src/main/java/com/smartschool/controller/AdminStatsServlet.java
@@ -6,36 +6,24 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import com.smartschool.dao.UserDAO;
 
 /**
  * Servlet implementation class AdminStatsServlet
  */
 @WebServlet("/AdminStatsServlet")
 public class AdminStatsServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AdminStatsServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        UserDAO dao = new UserDAO();
+        
+        // Build a JSON object manually or with Gson
+        String json = "{"
+            + "\"totalStudents\":" + dao.getTotalCount("student") + ","
+            + "\"totalTeachers\":" + dao.getTotalCount("teacher") + ","
+            + "\"pendingCount\":" + dao.getPendingValidationCount()
+            + "}";
+
+        response.setContentType("application/json");
+        response.getWriter().write(json);
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
