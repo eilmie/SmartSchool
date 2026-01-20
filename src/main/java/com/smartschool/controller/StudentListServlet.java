472564e21c2db13a384e110320ed076d8761a3d6
Muhammad Eilmie <muhammadeilmie@gmail.com>
2026-01-20 17:30:15 +0800

    add student 2

--- src/main/java/com/smartschool/controller/StudentListServlet.java
+++ src/main/java/com/smartschool/controller/StudentListServlet.java
@@ -15,7 +15,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/admin/StudentListServlet")
+@WebServlet(urlPatterns = {"/admin/StudentListServlet", "/admin/AddStudentServlet"})
 public class StudentListServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private UserDAO userDAO;
